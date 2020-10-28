b9a0e7705e2af665758695efd28a0f396578aaff
Krupin.SA <krupin.sergrey@gmail.com>
2020-10-28 10:32:10 +0500

    move main function to main.py

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# tm_messeger
+# tm_messenger
+
+The service is designed to notify in telegram about checking works on courses https://dvmn.org
+
+## Environment
+
+### Requirements
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+### Environment variables
+
+- SECRETS_KEY
+
+```sh
+export TELERAM_TOKEN="......"
+
+export TELEGRAM_CHAT_ID="......"
+
+export DEVMAN_TOKEN="....."
+```
+#### How to get
+
+* Follow the link for instructions on how to get a telegram token. https://core.telegram.org/bots#6-botfather
+
+## Run
+
+Launch on Linux(Python 3) or Windows:
+
+```sh
+$ python main.py
+```
--- main.py
+++ main.py
@@ -3,68 +3,111 @@ import requests
 import telegram
 import textwrap
 import time
-import teleg_logging
+import logging
 from telegram.ext import Updater
 
 
 DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
-TG_TOKEN = os.getenv('TG_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 WAITING_TIME = 91
 
-url = 'https://dvmn.org/api/long_polling/'
-
-headers = {
-    "Authorization": 'Token {}'.format(DEVMAN_TOKEN)
-}
-
-updater = Updater(token=TG_TOKEN, use_context=True)
+main_logger = logging.getLogger(__name__)
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-bot = telegram.Bot(token=TG_TOKEN)
-main_logger = teleg_logging.get_logger(__name__)
-dispatcher.logger.addHandler(teleg_logging.get_teleg_handler(bot, CHAT_ID))
-main_logger.warning("Bot запущен")
-dispatcher.logger.warning("Bot запущен")
-params = {}
-connection_error_count = 0
-    
-while True:
-  try:
-    response = requests.get(url, headers=headers, params=params, timeout=WAITING_TIME)
-    response.raise_for_status()
-    response_content = response.json()
-    if 'error' in response_content:
-        raise requests.exceptions.HTTPError(response_content['error'])
-    if response_content['status'] == 'timeout':
-        params = {'timestamp': response_content['timestamp_to_request']}
-        continue
-    if response_content['status'] == 'found':
-        params = {'timestamp': response_content['last_attempt_timestamp']} 
-        for attempt in response_content['new_attempts']:
-            text = '''\
-            У вас проверили работу "{}"
-            https://dvmn.org{}
-            '''
-            text = text.format(attempt['lesson_title'],
-                               attempt['lesson_url'],
-                            )
-            if attempt['is_negative']:
-                text += 'К сожалению, в работе нашлись ошибки.'
-            else:
-                text += 'Преподователю все понравилось, можно приступать к следующему уроку!'
-            text = textwrap.dedent(text)
-            bot.send_message(CHAT_ID, text=text)
+
+class TelegHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
+        
+    def emit(self, record):
+        log = self.format(record)
+        self.bot.send_message(self.chat_id, log)
+
+
+def init_bot():
+    return telegram.Bot(token=TELEGRAM_TOKEN)
+
+
+def main_loop(bot):
+    url = 'https://dvmn.org/api/long_polling/'
+
+    headers = {
+        "Authorization": f'Token {DEVMAN_TOKEN}'
+    }
+
+    params = {}
     connection_error_count = 0
         
-  except requests.exceptions.ReadTimeout: pass
-  except requests.exceptions.ConnectionError: 
-      if connection_error_count == 5:
-          time.sleep(600)
-          continue
-      connection_error_count += 1
-  except telegram.error.TelegramError as err:
-      main_logger.error(err)
-      dispatcher.logger.error("Bot произошла ошибка")
-      dispatcher.logger.error(err, exc_info=True)
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=params, timeout=WAITING_TIME)
+            response.raise_for_status()
+            response_content = response.json()
+            if 'error' in response_content:
+                raise requests.exceptions.HTTPError(response_content['error'])
+            if response_content['status'] == 'timeout':
+                params = {'timestamp': response_content['timestamp_to_request']}
+                continue
+            if response_content['status'] == 'found':
+                params = {'timestamp': response_content['last_attempt_timestamp']} 
+                for attempt in response_content['new_attempts']:
+                    text = '''\
+                    У вас проверили работу "{}"
+                    https://dvmn.org{}
+                    '''
+                    text = text.format(attempt['lesson_title'],
+                                    attempt['lesson_url'],
+                                    )
+                    if attempt['is_negative']:
+                        text += 'К сожалению, в работе нашлись ошибки.'
+                    else:
+                        text += 'Преподователю все понравилось, можно приступать к следующему уроку!'
+                    text = textwrap.dedent(text)
+                    bot.send_message(TELEGRAM_CHAT_ID, text=text)
+            connection_error_count = 0
+                
+        except requests.exceptions.ReadTimeout: pass
+        except requests.exceptions.ConnectionError: 
+            if connection_error_count == 5:
+                time.sleep(600)
+                continue
+            connection_error_count += 1
+        except telegram.error.TelegramError as err:
+            main_logger.error(err)
+            dispatcher.logger.error("Bot произошла ошибка")
+            dispatcher.logger.error(err, exc_info=True)
+
+
+def main():
+    message_format = "%(asctime)s:[%(name)s]%(filename)s.%(funcName)s:%(levelname)s:%(message)s"
+    bot_message_format = "%(message)s"
+    level = logging.INFO
+
+    # Init logging to console
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter(message_format)
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(level)
+    main_logger.addHandler(console_handler)
+
+    bot = init_bot()
+    main_logger.warning("Bot запущен")
+
+    # Init ligging to telegram
+    telegram_handler = TelegHandler(bot, TELEGRAM_CHAT_ID)
+    formatter = logging.Formatter(bot_message_format)
+    telegram_handler.setFormatter(formatter)
+    telegram_handler.setLevel(level)
+    dispatcher.logger.addHandler(telegram_handler)
+
+    dispatcher.logger.warning("Bot запущен")
+
+    main_loop(bot)
+
+if __name__ == "__main__":
+    main()
  
